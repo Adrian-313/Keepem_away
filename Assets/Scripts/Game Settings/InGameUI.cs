b3a3aab305a0ca0e48bc59eaee5edd0492fbff0d
carloshv31 <carlos.heve@gmail.com>
2025-03-30 17:50:51 -0500

    Fixed bug with Game Over screen

--- Assets/Scripts/Game Settings/InGameUI.cs
+++ Assets/Scripts/Game Settings/InGameUI.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 public class InGameUI : MonoBehaviour
 {
     public GameObject gameOverScreen;
+    public GameObject visibleIndicators;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         gameOverScreen.gameObject.SetActive(false);
+        visibleIndicators.gameObject.SetActive(true);
     }
 
     public void ReloadGame()
@@ -35,6 +37,7 @@ public class InGameUI : MonoBehaviour
         if (GameManager.Instance.isGameOver)
         {
             gameOverScreen.gameObject.SetActive(true);
+            visibleIndicators.gameObject.SetActive(false);
         }
     }
 }
