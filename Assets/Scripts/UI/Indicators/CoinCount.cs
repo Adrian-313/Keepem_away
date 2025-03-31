934c11c29ee82c90b28a85abdc4f3a49c61dfa5b
carloshv31 <carlos.heve@gmail.com>
2025-03-31 14:30:36 -0500

    Added visual indicators for power-ups and kills

--- Assets/Scripts/UI/Indicators/CoinCount.cs
+++ Assets/Scripts/UI/Indicators/CoinCount.cs
@@ -5,6 +5,7 @@ public class CoinCount : MonoBehaviour
 {
     public int coins = 0;
     public TextMeshProUGUI coinText; // Referencia al texto de la UI
+    public int coinCost = 20; // Cantidad de monedas a descontar
 
     void Start()
     {
@@ -17,6 +18,20 @@ public class CoinCount : MonoBehaviour
         UpdateCoinUI();
     }
 
+    public void SpendCoins()
+    {
+        if (coins >= coinCost)
+        {
+            coins -= coinCost;
+            UpdateCoinUI();
+            Debug.Log("Has gastado " + coinCost + " monedas.");
+        }
+        else
+        {
+            Debug.Log("No tienes suficientes monedas.");
+        }
+    }
+
     void UpdateCoinUI()
     {
         coinText.text = "x" + coins;
@@ -27,6 +42,10 @@ public class CoinCount : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.C))
         {
             AddCoin();
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            SpendCoins();
         }
     }
 }
