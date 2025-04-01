8bc5731b5b867e8fd813ef4ff231394bbdef619b
Julian Santiago Diaz <107156927+juliansdiaz@users.noreply.github.com>
2025-04-01 10:18:54 -0500

    Modified Level Design and Physical boundaries

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float dashCooldown = 1f;
 
     public float playerHealth = 100;
+    public float coinScore = 500f;
     private bool canMove = true;
     private Rigidbody rb;
     private Vector3 moveDirection;
