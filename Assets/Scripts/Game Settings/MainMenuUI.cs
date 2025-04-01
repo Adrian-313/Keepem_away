3c4b851328e6588bbf5e0476dbd2954f2e38d399
carloshv31 <carlos.heve@gmail.com>
2025-04-01 17:40:00 -0500

    Added music and sfx and modified audio and game settings scripts

--- Assets/Scripts/Game Settings/MainMenuUI.cs
+++ Assets/Scripts/Game Settings/MainMenuUI.cs
@@ -6,6 +6,12 @@ public class MainMenuUI : MonoBehaviour
     public void NextScene()
     {
         //Debug.Log("Cambio de escena");
+        AudioManager.Instance.PlayMusic("Game Theme");
         GameManager.Instance.ChangeScene("julian");
+    }
+
+    public void MenuButtonPressed()
+    {
+        AudioManager.Instance.PlaySFX("Button Pressed");
     }
 }
